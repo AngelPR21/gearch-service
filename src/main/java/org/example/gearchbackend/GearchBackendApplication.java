b74dd7c8d772560106685bfb8587ce74cf2bf7c4
apascualr <angpasrod2@alu.edu.gva.es>
2026-01-09 15:29:53 +0100

    v0

---
+++ src/main/java/org/example/gearchbackend/GearchBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.gearchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearchBackendApplication.class, args);
+	}
+
+}
