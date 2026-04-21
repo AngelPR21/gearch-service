b1a9826a72d732a342cc8531aba02369d17e3445
AngelPR21 <angelpragosto210806@gmail.com>
2026-04-21 10:05:47 +0200

    v0

--- src/main/java/com/gearch/gearchbackend/services/VehiculoService.java
+++ src/main/java/com/gearch/gearchbackend/services/VehiculoService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class VehiculoService {
+public class  VehiculoService {
 
     private final VehiculoRepository vehiculoRepository;
     private final UsuarioRepository usuarioRepository;
